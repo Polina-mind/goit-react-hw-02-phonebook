c2ad6c45abe09e9d3928fdcca7a1e4c3f846d23c
Polina-mind <popova.v.polina@gmail.com>
2021-04-19 19:40:58 +0300

    update

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -4,13 +4,24 @@ import InputForm from '../InputForm';
 
 class Form extends Component {
   state = {
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
     name: '',
     number: '',
   };
 
   onInputValue = event => {
     const { name, value } = event.currentTarget;
-    this.setState({ [name]: value });
+
+    if (this.state.contacts.find(contact => contact.name !== value)) {
+      this.setState({ [name]: value });
+    } else {
+      alert(name, `is already in contacts`);
+    }
   };
 
   onSubmit = event => {
